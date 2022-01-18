d07e12a1faa1ec61e16fb07ad800a13139e7cb3e
Tanishk Goyal <goyaltanishk02@gmail.com>
2022-01-18 13:39:39 +0530

    Getting Balance

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ const {
     Transaction,
     Account,
    } = require("@solana/web3.js");
+
+   const newPair = new Keypair();
+   const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+   const privateKey = new newPair._keypair.secretKey;
+
+   const getWalletBalance = async() =>{
+       try{
+            const connection =  new Connection(clusterApiUrl("devnet"), "confirmed");
+            const wallet = await Keypair.fromSecretKey(privateKey);
+            const balance = await connection.getBalance()(new PublicKey(wallet.publicKey));   
+            console.log("this is the balance: $(balance)");
+       }
+       catch(err){
+           console.log(err);
+
+       }
+   }
